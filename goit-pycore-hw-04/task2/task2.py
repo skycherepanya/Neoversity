fe64762c252d30436b3997b0fc0e8fa91deed307
skycherepanya <skycherepanya@gmail.com>
2025-10-21 18:09:31 +0200

    Error handling

--- goit-pycore-hw-04/task2/task2.py
+++ goit-pycore-hw-04/task2/task2.py
@@ -1,17 +1,20 @@
 def get_cats_info(path):
-    with open(path, "r") as file:
-        text = [el.strip() for el in file.readlines()]
-        cats = []
-        
-        for cat in text:
-            splited = cat.split(',')
-            cat_obj = {
-                "id" : splited[0],
-                "name" : splited[1],
-                "age" : splited[2]
-            }
-            cats.append(cat_obj)
-        return cats
+    try:
+        with open(path, "r") as file:
+            text = [el.strip() for el in file.readlines()]
+            cats = []
+            
+            for cat in text:
+                splited = cat.split(',')
+                cat_obj = {
+                    "id" : splited[0],
+                    "name" : splited[1],
+                    "age" : splited[2]
+                }
+                cats.append(cat_obj)
+            return cats
+    except FileNotFoundError:
+        return "Не вдалося знайти файл."
 
 cats_info = get_cats_info("task2/cats_file.txt")
 print(cats_info)
